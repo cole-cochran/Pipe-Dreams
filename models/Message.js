883af938e21407492046a6905951a3aa3d0d7051
Mat Lundin <matmll12@gmail.com>
2021-11-01 10:50:39 -0500

    user and message models

---
+++ models/Message.js
@@ -0,0 +1,49 @@
+const { Model, DataTypes } = require('sequelize');
+const bcrypt = require('bcrypt');
+const sequelize = require('../config/connection');
+const User = require('./User')
+
+class Message extends Model {}
+
+Message.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    date: {
+      type: DataTypes.DATETIME,
+      allowNull: false,
+    },
+    text: {
+      type: DataTypes.TEXT,
+    //   allowNull: false,
+    },
+    image_url: {
+      type: DataTypes.STRING,
+      validate: {
+        isUrl,
+      },
+    },
+    user_id: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        references: {
+            model: User,
+            key: 'id'
+        }
+    }
+  },
+  {
+
+    sequelize,
+    timestamps: false,
+    freezeTableName: true,
+    underscored: true,
+    modelName: 'user',
+  }
+);
+
+module.exports = User;

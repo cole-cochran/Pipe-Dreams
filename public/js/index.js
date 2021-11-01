ab4f6255e90912edeb7a5876e997d42873a12962
Mat Lundin <matmll12@gmail.com>
2021-11-01 13:07:20 -0500

    js event listener, linked to main page

--- public/js/index.js
+++ public/js/index.js
@@ -1,4 +1,6 @@
 //event handlers for homepage
+const searchBtn = document.getElementById()
+const histBtn = document.getElementById()
 
 function getLocation() {
     if (navigator.geolocation) {
@@ -6,9 +8,17 @@ function getLocation() {
     } else {
       console.log("Geolocation is not supported by this browser.");
     }
-  }
+};
   
-  function showPosition(position) {
+function showPosition(position) {
     console.log("Latitude: " + position.coords.latitude +
     "<br>Longitude: " + position.coords.longitude);
-  }
+};
+
+
+//replace with API function
+searchBtn.addEventListener('click', function(event) {
+    event.preventDefault();
+
+
+})

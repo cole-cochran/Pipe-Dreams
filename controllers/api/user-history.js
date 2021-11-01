ac852ae63ff3123404985eda07495682e4b909d8
WTRIII <thomren3@gmail.com>
2021-11-01 11:19:24 -0500

    moved public folder outside of models. added api routes that link to models.

---
+++ controllers/api/user-history.js
@@ -0,0 +1,85 @@
+const router = require('express').Router();
+const UserHistory = require('../../models/User_history');
+
+// The `/api/categories` endpoint
+
+//find all UserHistory
+router.get('/', async (req, res) => {
+  try {
+    const favoriteData = await UserHistory.findAll()
+    res.status(200).json(favoriteData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+// find favorite by 'id' - can be changed to location? or name?
+router.get('/:id', async (req, res) => {
+  try {
+    const favoriteData = await UserHistory.findByPk(req.params.id, {
+      include: [{ model: Product }]
+    });
+
+    if (!favoriteData) {
+      res.status(404).json({ message: 'No favorite found!' });
+      return;
+    }
+
+    res.status(200).json(favoriteData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+
+});
+
+// save a new favorite
+router.post('/', async (req, res) => {
+  try {
+    const favoriteData = await UserHistory.create(req.body);
+    res.status(200).json(favoriteData);
+  } catch (err) {
+    res.status(400).json(err);
+  }
+});
+
+//edit favorite by 'id'
+router.put('/:id', async (req, res) => {
+  try {
+    const favoriteData = await UserHistory.update(req.body, {
+      where: {
+        id: req.params.id,
+      },
+    });
+    if (!favoriteData[0]) {
+      res.status(404).json({ message: 'No favorite with this id!' });
+      return;
+    }
+    res.status(200).json(favoriteData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+// delete a message by id - will need to connect delete button to message id
+// need to make sure only user who made the comment or admin can delete
+router.delete('/:id', async (req, res) => {
+  try {
+    const favoriteData = await UserHistory.destroy({
+      where: {
+        id: req.params.id
+      }
+    });
+
+    if (!favoriteData) {
+      res.status(404).json({ message: 'No favorite found with this id!' });
+      return;
+    }
+
+    res.status(200).json(favoriteData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+
+});
+
+module.exports = router;
---
+++ controllers/api/user.js
@@ -0,0 +1,83 @@
+const router = require('express').Router();
+const User = require('../../models/User');
+
+// The `/api/categories` endpoint
+
+// find all users
+router.get('/', async (req, res) => {
+  try {
+    const userData = await User.findAll()
+    res.status(200).json(userData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+// find one user by its `id` value
+router.get('/:id', async (req, res) => {
+  try {
+    const userData = await User.findByPk(req.params.id);
+
+    if (!userData) {
+      res.status(404).json({ message: 'No user found with this id!' });
+      return;
+    }
+
+    res.status(200).json(userData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+
+});
+
+// create a new user
+router.post('/', async (req, res) => {
+  try {
+    const userData = await User.create(req.body);
+    res.status(200).json(userData);
+  } catch (err) {
+    res.status(400).json(err);
+  }
+});
+
+//update a user's information
+router.put('/:id', async (req, res) => {
+  try {
+    const userData = await User.update(req.body, {
+      where: {
+        id: req.params.id,
+      },
+    });
+    if (!userData[0]) {
+      res.status(404).json({ message: 'No user with this id!' });
+      return;
+    }
+    res.status(200).json(userData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+//delete a user by id
+// 'archive' user instead, like what FB does?
+router.delete('/:id', async (req, res) => {
+  try {
+    const userData = await User.destroy({
+      where: {
+        id: req.params.id
+      }
+    });
+
+    if (!userData) {
+      res.status(404).json({ message: 'No user found with this id!' });
+      return;
+    }
+
+    res.status(200).json(userData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+
+});
+
+module.exports = router;

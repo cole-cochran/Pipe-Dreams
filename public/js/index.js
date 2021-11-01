62f53335abfa389f4e0a6c2a7bd7097800ce3831
Mat Lundin <matmll12@gmail.com>
2021-11-01 12:02:49 -0500

    added geo location to index

--- public/js/index.js
+++ public/js/index.js
@@ -1,1 +1,14 @@
 //event handlers for homepage
+
+function getLocation() {
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(showPosition);
+    } else {
+      console.log("Geolocation is not supported by this browser.");
+    }
+  }
+  
+  function showPosition(position) {
+    console.log("Latitude: " + position.coords.latitude +
+    "<br>Longitude: " + position.coords.longitude);
+  }

f987d0832a2333ec4445c0f70baef90578f347bb
Mat Lundin <matmll12@gmail.com>
2021-11-02 14:09:02 -0500

    got socket io to work on our index page

--- server.js
+++ server.js
@@ -9,6 +9,7 @@ const http = require('http');
 const server = http.createServer(app);
 const { Server } = require("socket.io");
 const io = new Server(server);
+// const io = require('socket.io').listen(server);
 
 const sequelize = require('./config/connection');
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
@@ -41,9 +42,28 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(routes);
 
-sequelize.sync({ force: false }).then(() => {
-  app.listen(PORT, () => console.log('Now listening'));
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/index.html');
 });
 
 
+io.on('connection', (socket) => {
+  console.log('a user connected!');
+});
+
+io.on('connection', (socket) => {
+  socket.on('chat message', (msg) => {
+    console.log('message: ' + msg);
+    io.emit('chat message', msg);
+
+  });
+});
+
+sequelize.sync({ force: false }).then(() => {
+  server.listen(PORT, () => console.log('Now listening'));
+});
+
+// server.listen(PORT, () => {
+//   console.log('listening on *:3001');
+// });
 

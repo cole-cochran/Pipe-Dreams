015e2eedf01294278585d1ac406e42889328ec49
Cole Cochran <colecochran405@gmail.com>
2021-11-03 14:54:55 -0500

    removing app.use as mat's mustache has the fix in his code

--- server.js
+++ server.js
@@ -41,7 +41,6 @@ app.set('view engine', 'handlebars');
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
-app.use(express.static(__dirname + '/public/css/style.css'))
 
 app.use(routes);
 

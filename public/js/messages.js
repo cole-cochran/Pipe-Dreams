faa899b3f09f576c71d99ff0acf637ecb81abef2
Mat Lundin <matmll12@gmail.com>
2021-11-03 13:19:51 -0500

    do not stringify axiom post bodies

--- public/js/messages.js
+++ public/js/messages.js
@@ -8,10 +8,10 @@ async function archiveMsg(msg) {
         // const response = await 
         axios.post('http://localhost:3001/api/messages', 
         //   headers: { 'Content-Type': 'application/json' },
-          JSON.stringify({
+          {
               user_id: 1,
               text: msg,
-          })
+          }
         ).then(function(response){
             console.log('response = ',response)
         }).catch((err) => {

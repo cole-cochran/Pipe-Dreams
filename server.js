546578f14a742ca71f53d2085bfa30a2cd6bf9a0
Mat Lundin <matmll12@gmail.com>
2021-11-03 14:46:08 -0500

    public folder imports, login link

--- server.js
+++ server.js
@@ -40,7 +40,10 @@ app.set('view engine', 'handlebars');
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, '/public')));
+app.use('*/css',express.static('public/css'));
+app.use('*/js',express.static('public/js'));
+app.use('*/images',express.static('public/images'));
 
 app.use(routes);
 
